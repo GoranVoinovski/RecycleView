a78fe3c4284e32edd52777021ec6412e1ffb171c
goran <gogovoinovski@gmail.com>
2017-12-07 18:35:40 +0100

    Adapter

---
+++ app/src/main/java/com/example/goran/recycleview/CustomAdapter.java
@@ -0,0 +1,57 @@
+package com.example.goran.recycleview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by goran on 7.12.17.
+ */
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+
+    List<Student> studentList = new ArrayList<>();
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(R.layout.recycler__view_row, parent, false);
+
+        ViewHolder holder = new ViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return studentList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        @BindView(R.id.textv)TextView mojtekst;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+
+
+        }
+    }
+}
